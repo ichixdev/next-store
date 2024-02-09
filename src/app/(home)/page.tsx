005568cbf00690692fa86b8b0b15967b08935a0a
Luis Moncada <luiismoncada@gmail.com>
2024-02-09 04:35:39 -0400

    metada for seo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,15 @@
-import { MainProducts } from "app/components/home/MainProducts"
+import { MainProducts } from 'app/components/home/MainProducts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Future World',
+  description: 'Welcome to the future world',
+};
 
 export default function Home() {
   return (
     <main>
       <MainProducts />
     </main>
-  )
+  );
 }
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -8,6 +8,21 @@ interface ProductPageProps {
   };
 }
 
+export async function generateMetadata({ searchParams }: ProductPageProps) {
+  const id = searchParams.id;
+  const products = await getProducts(id);
+  const product = products[0];
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image],
+    },
+  };
+}
+
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
