739860f2494a7acb4e80c398fc9496beb459ffd5
Luis Moncada <luiismoncada@gmail.com>
2024-02-20 05:14:56 -0400

    cookies access token

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,30 @@
 'use server';
 
-export const handleCreateUser = (formData: any) => {
-  console.log('hanldeCreateUser', formData);
+import { GraphQLClientSingleton } from 'app/graphql';
+import { createUserMutation } from 'app/graphql/mutations/createUserMutation';
+import { createAccessToken } from 'app/utils/auth/createAccessToken';
+import { redirect } from 'next/navigation';
+
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData);
+  delete formDataObject['password_confirmation'];
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient();
+  const varialbles = {
+    input: {
+      ...formDataObject,
+      phone: '+58' + formDataObject.phone,
+    },
+  };
+  const { customerCreate } = await graphqlClient.request(
+    createUserMutation,
+    varialbles
+  );
+  const { customer } = customerCreate;
+  if (customer?.firtsName) {
+    await createAccessToken(
+      formDataObject.email as string,
+      formDataObject.password as string
+    );
+    redirect('/store');
+  }
 };
